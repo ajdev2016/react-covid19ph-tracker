d01792ccb1e908a795d5f9dd7281b677db7b86c3
Aj Nocolai Garrigues <ajgarrigues@rocketmail.com>
2020-04-08 16:59:34 +0800

    WIP add api call in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
-import { Button } from 'reactstrap';
+import axios from "axios";
 
 export class Home extends Component {
+    
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            covid_data: null    
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://coronavirus-ph-api.herokuapp.com/cases')
+        .then( response => {
+            // console.log({response});
+            this.setState({covid_data: response.data})
+        })
+        .catch((e) => {
+            console.log(e);
+        })  
+    }
+
     render() {
+        const {covid_data} = this.state
+        console.log({covid_data})
         return (
             <div>
-                <Button color="primary">test button</Button>{' '}
+                
             </div>
         )
     }
