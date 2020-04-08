e8c450dc723bf495bb5f82216734e94a55b8178e
Aj Nocolai Garrigues <ajgarrigues@rocketmail.com>
2020-04-08 14:16:50 +0800

    WIP covid tracker template

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Button color="primary">test button</Button>{' '}
+            </div>
+        )
+    }
+}
+
+export default Home
